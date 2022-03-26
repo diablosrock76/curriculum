014159e0079a83081f4e2f24b4a399c1bfe4eacb
diablosrock76 <diablosrock.76@gmail.com>
2022-03-26 09:16:51 +0900

    再課題提出
    お疲れさまです。
    
    課題の評価をお願いします。
    レポジトリURL
    https://github.com/diablosrock76/curriculum.git

--- 2-9/index.php
+++ 2-9/index.php
@@ -1,11 +1,11 @@
 <?php
 for ($Numbers = 1; $Numbers <= 100; $Numbers++) {
-    if ($Numbers % 3 === 0) {
+    if ($Numbers % 3=== 0 && $Numbers % 5 ===0) {
+        echo 'FizzBuzz!!';
+    }elseif ($Numbers % 3 === 0) {
         echo 'Fizz!';
     }elseif ($Numbers % 5 === 0) {
-        echo 'Buzz!';
-    }elseif ($Numbers % 3=== 0 && $Numbers % 5 ===0) {
-        echo 'FizzBuzz!!';
+            echo 'Buzz!';
     }else {
         echo $Numbers; 
     }
