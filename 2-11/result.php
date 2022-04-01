c58a3ab04865da8caac0b41b977109510296ee62
diablosrock76 <diablosrock.76@gmail.com>
2022-04-01 17:01:28 +0900

    課題提出
    お疲れさまです。
    
    課題の評価をお願いします。
    レポジトリURL
    https://github.com/diablosrock76/curriculum.git

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$email_address = $_POST['email_address'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $email_address; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
