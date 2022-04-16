449b3beb84be47701a07f7dde18831969275fd1d
diablosrock76 <diablosrock.76@gmail.com>
2022-04-16 11:42:37 +0900

    課題の再提出
    お疲れ様です。
    
    課題を再提出します。
    評価をお願いします。
    レポジトリURLhttps://github.com/diablosrock76/curriculum.git

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -3,29 +3,20 @@ $my_name = $_POST['my_name'];
 
 $question1 = $_POST['question1']; 
 $answer1 = $_POST['answer1'];   
-if($question1 == $answer1){
-	$result1 = "正解！";
-}else{
-	$result1 = "不正解･･･";
-}
 
 $question2 = $_POST['question2']; 
 $answer2 = $_POST['answer2'];   
-if($question2 == $answer2){
-	$result2 = "正解！";
-}else{
-	$result2 = "不正解･･･";
-}
 
 $question3 = $_POST['question3']; 
 $answer3 = $_POST['answer3'];   
-if($question3 == $answer3){
-	$result3 = "正解！";
-}else{
-	$result3 = "不正解･･･";
-}
 
-
+function judgeAnswer($qes, $ans){
+  if($qes == $ans){
+  echo 正解！;
+  }else{
+  echo 不正解･･･;
+  }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,10 +30,10 @@ if($question3 == $answer3){
 <body>
 <p><?php echo $my_name;?>さんの結果は・・・？</p>
 <p>①の答え</p>
-<?php echo $result1 ?>
+<?php judgeAnswer($question1,$answer1); ?>
 <p>②の答え</p>
-<?php echo $result2 ?>
+<?php judgeAnswer($question2,$answer2); ?>
 <p>③の答え</p>
-<?php echo $result3 ?>
+<?php judgeAnswer($question3,$answer3); ?>
 </body>
 </html>
